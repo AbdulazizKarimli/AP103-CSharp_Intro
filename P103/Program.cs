49b52c4da9a6b9a25ef7632e50da2cd267917205
Abdulaziz930 <abdulazizak@code.edu.az>
2022-03-03 14:12:22 +0400

    first commit

---
+++ P103/Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+
+namespace P103
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Variables
+            //int age = 25;
+            //Console.WriteLine(age);
+            //age++;
+            //Console.WriteLine(age);
+
+            //string word = "lorem";
+            //Console.WriteLine(word);
+            //word = "4566";
+            //Console.WriteLine(word);
+
+            //bool isMarried = true;
+            //Console.WriteLine(isMarried);
+            //isMarried = false;
+            //Console.WriteLine(isMarried);
+
+            //double price = 4.5;
+            //price = 3;
+            //Console.WriteLine(price);
+
+            //decimal currency = 5.5m;
+            //currency = 2;
+            //Console.WriteLine(currency);
+
+            //float costPrice = 4.5f;
+            //Console.WriteLine(costPrice);
+
+            //dynamic number = 1;
+            //number = "";
+            //number = true;
+
+            //var number = 5;
+            //number = 6;
+
+
+
+
+            #endregion
+
+            #region conditions
+
+            //int age =23;
+            //if (age>25)
+            //{
+            //    Console.WriteLine("icaze var");
+            //}
+            //else if (age==25)
+            //{
+            //    Console.WriteLine("vesiqeni goster");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("icaze yoxdur");
+            //}
+
+            //int day = 6;
+            //switch (day)
+            //{
+            //    case 1:
+            //        Console.WriteLine("1 ci gundur");
+            //        break;
+
+            //    case 2:
+            //        Console.WriteLine("2ci gundur");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("3 ci gundur");
+            //        break;
+
+            //    case 4:
+            //        Console.WriteLine("4ci gundur");
+            //        break;
+
+            //    case 5:
+            //    case 6:
+            //        Console.WriteLine("ya besdir yada 6");
+            //        break;
+
+
+            //    default:
+            //        Console.WriteLine("heftede 4gun var");
+            //        break;
+            //}
+
+            #endregion
+
+            #region loops
+            //int sum = 0;
+            //for (int i = 0; i <= 10; i++)
+            //{
+            //    int age = 15;
+            //    sum += i;
+            //}
+            //Console.WriteLine(sum);
+
+            int[] numArr = { 1, 2, 5, 9, 7 };
+            string[] wordArr = { "Ehmed", "Lorem", "Ipsum" };
+
+            //for (int i = 0; i < numArr.Length; i++)
+            //{
+            //    Console.WriteLine(numArr[i]);
+            //}
+            //foreach (var item in wordArr)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //int i = 0;
+            //while (i<10)
+            //{
+            //    i++;
+            //    Console.WriteLine(i);
+            //}
+            //while (true)
+            //{
+            //    Console.WriteLine("lorwem");
+            //}
+            //for (; ; )
+            //{
+
+            //}
+            //int i = 1;
+            //do
+            //{
+            //    Console.WriteLine(i);
+            //} while (i < 0);
+            #endregion
+
+            #region task
+            //int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            //int sum = 0;
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    if (numbers[i] % 2 == 0)
+            //    {
+            //        sum = sum + numbers[i];
+            //    }
+            //}
+            //foreach (var item in numbers)
+            //{
+            //    if (item % 2 == 0)
+            //    {
+            //        sum = sum + item;
+            //    }
+            //}
+
+            //Console.WriteLine(sum);
+            #endregion
+        }
+    }
+}
